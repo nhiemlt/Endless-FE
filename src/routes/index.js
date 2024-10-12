ac357fb70677a1459bfd514585e800f3fe6dedf5
vtthaonguyen <149494249+vtthaonguyen@users.noreply.github.com>
2024-10-12 09:21:01 +0700

    thêm và sửa trong thư mục src -> pages -> protected và routes -> index

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,12 +18,81 @@ const GettingStarted = lazy(() => import('../pages/GettingStarted'))
 const DocFeatures = lazy(() => import('../pages/DocFeatures'))
 const DocComponents = lazy(() => import('../pages/DocComponents'))
 const Attribute = lazy(() => import('../pages/protected/Attribute'))
-
+const AttributeValue = lazy(() => import('../pages/protected/AttributeValue'))
+const Brand = lazy(() => import('../pages/protected/Brand'))
+const Category = lazy(() => import('../pages/protected/Category'))
+const Entry = lazy(() => import('../pages/protected/Entry'))
+const Notification = lazy(() => import('../pages/protected/Notification'))
+const Product = lazy(() => import('../pages/protected/Product'))
+const ProductVersion = lazy(() => import('../pages/protected/ProductVersion'))
+const Promotion = lazy(() => import('../pages/protected/Promotion'))
+const Rating = lazy(() => import('../pages/protected/Rating'))
+const Role = lazy(() => import('../pages/protected/Role'))
+const StatisticalInvendory = lazy(() => import('../pages/protected/StatisticalInvendory'))
+const StatisticalProduct = lazy(() => import('../pages/protected/StatisticalProduct'))
+const StatisticalRevenue = lazy(() => import('../pages/protected/StatisticalRevenue'))
+const Voucher = lazy(() => import('../pages/protected/Voucher'))
 
 const routes = [
   {
     path: '/attribute', // the url
     component: Attribute, // view rendered
+  },
+  {
+    path: '/attribute-value', // the url
+    component: AttributeValue, // view rendered
+  },
+  {
+    path: '/brand', // the url
+    component: Brand, // view rendered
+  },
+  {
+    path: '/category', // the url
+    component: Category, // view rendered
+  },
+  {
+    path: '/entry', // the url
+    component: Entry, // view rendered
+  },
+  {
+    path: '/notification', // the url
+    component: Notification, // view rendered
+  },
+  {
+    path: '/product', // the url
+    component: Product, // view rendered
+  },
+  {
+    path: '/product-version', // the url
+    component: ProductVersion, // view rendered
+  },
+  {
+    path: '/promotion', // the url
+    component: Promotion, // view rendered
+  },
+  {
+    path: '/rating', // the url
+    component: Rating, // view rendered
+  },
+  {
+    path: '/role', // the url
+    component: Role, // view rendered
+  },
+  {
+    path: '/statistical-invendory', // the url
+    component: StatisticalInvendory, // view rendered
+  },
+  {
+    path: '/statistical-product', // the url
+    component: StatisticalProduct, // view rendered
+  },
+  {
+    path: '/statistical-revenue', // the url
+    component: StatisticalRevenue, // view rendered
+  },
+  {
+    path: '/voucher', // the url
+    component: Voucher, // view rendered
   },
   {
     path: '/dashboard', // the url
